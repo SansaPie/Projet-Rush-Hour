6cab4ac10d2fd90e1e87ec1c5a3071413e23364b
SansaPie <lisa@arneau.net>
2016-03-12 20:12:33 +0100

    La fonction lecture fonctionne, possibilité de laisser le joueur choisir à quelle config il veut jouer. Je vais créer différentes config qu'il pourra demander à lire. Je modifie le main pour permettre la lecture de fichier dans le jeu.

--- lecture.c
+++ lecture.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include <stdbool.h>
 #include "piece.c"
 #include "game.c"
 #define NB_PIECE_TEST_RH 5
 
-// Fonction lecture de fichier pour une configuration de n'importe quel type 
-// coordonnée x / coordonnée y / bool move_x / bool move_y / largeur / hauteur
 
-void lecture(piece pieces_test[], int * n);
-
+void lecture(piece pieces_test[], int * n, FILE * entree);
 
 int number_pieces; // nombre de pièces
 int c_x;	   // coordonnée x
@@ -20,12 +18,54 @@ int m_y;	   // bool move_y
 int w;		   // int largeur
 int h;		   // int hauteur
 
+
+// Fonction lecture de fichier pour une configuration de n'importe quel type 
+// coordonnée x / coordonnée y / bool move_x / bool move_y / largeur / hauteur
+
+char * scan(char * buffer , int size) {
+	char * result = fgets(buffer, size, stdin);
+	/* on cherche dans la chaine de caractere saisie le caractere d'echappement pour le remplacer par un 
+	 caractere de fin de fichier. */
+	if ( result != NULL) {
+		char * lresult = strchr(buffer, '\n');
+		if (lresult != NULL)
+			*lresult = '\0';
+	}
+	rewind(stdin); /* on se place au debut du flux d'entree */
+	return result;
+}
+
+
+void choix_config(piece pieces_test[], int * n, char * answer)
+{
+	if (strcmp(answer, "configurations.txt") == 0)
+	{
+		FILE *entree = fopen("configurations.txt", "r+");
+		entree = fopen("configurations.txt", "r+");
+		lecture(pieces_test, n, entree);
+	}
+}
+
+
 int main(void)
 {
+	int size = 30;
+	char * answer = malloc(sizeof(char)*size);
 	piece * pieces_test = NULL;
 	pieces_test = allocation_piece_tab(NB_PIECE_TEST_RH, "main"); /* on cree un tableau qui contient les pieces */
 	int n = 0;
-	lecture(pieces_test, &n);
+	printf("La liste des configurations disponibles est : \n"
+		"	srgrgq.txt\n	sfnsdfi.txt\n	irnqgirgn.txt\n \nVeuillez recopier le nom du fichier que vous souhaitez utiliser. \n");
+	scan(answer, size);
+	if (answer != NULL) 
+	{ 
+    		char * p = strchr(answer, '\n'); 
+  
+    		if (p != NULL) 
+        	*p = 0; 
+	}
+	choix_config(pieces_test, &n, answer);
+	// lecture(pieces_test, &n);
 	for (int x=0; x<n; x++)
 	{
 		printf("%d %d %d %d %d %d \n", pieces_test[x]->x,  pieces_test[x]->y,  pieces_test[x]->move_x,  pieces_test[x]->move_y,  pieces_test[x]->width,  pieces_test[x]-> height);
@@ -33,9 +73,10 @@ int main(void)
 	return EXIT_SUCCESS;
 }
 
-void lecture(piece pieces_test[], int * n) {
-	FILE *entree;
-	entree = fopen("configurations.txt", "r+");
+
+
+void lecture(piece pieces_test[], int * n, FILE * entree) 
+{
 	if( entree == NULL )
 	{
 		printf("error when opening configurations.txt\n");
