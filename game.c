66968e3d020ddb6cb0bb836f7eb5ba6a3466b806
Lucas <tiresias@ubuntu.(none)>
2016-03-17 10:08:57 +0100

    modif move_piece, la sécurité sur la direction du mouvement doit se trouver dans cette fonction et non dans play_move

--- game.c
+++ game.c
@@ -146,10 +146,7 @@ bool play_move(game g, int piece_num, dir d, int distance){
 	piece piece_moved = gTmp->pieces[piece_num];
 	
 	for (int i = 0; i < distance; i++) {
-		if (((d == LEFT || d == RIGHT) && can_move_x(piece_moved)) ||
-			((d == UP || d == DOWN) && can_move_y(piece_moved))) {
-			move_piece(piece_moved, d, 1);
-		}
+		move_piece(piece_moved, d, 1);
 		if (!game_valid(gTmp)) {
 			delete_game(gTmp);
 			delete_pieces(1,t_pieces);
