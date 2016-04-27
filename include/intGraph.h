cc8e6da0c8b0fc0afe6ae6c7699ce6b31d2d94e7
Lucas <tiresias@ubuntu.(none)>
2016-04-27 22:30:03 +0200

    rectification header intGraph et CMakeLists.txt, ajout possibilité afficher les règles du jeu, sprites à venir

--- include/intGraph.h
+++ include/intGraph.h
@@ -16,27 +16,32 @@ void is_a_piece(cgame g, int * piece_selected, int nb);
 /**
  * @brief Displays the hovered piece.
  */
-void display_piece_hovered(game g, SDL_Surface * screen, char game_type, int piece_hovered, SDL_Rect position_cursor);
+void display_piece_hovered(cgame g, SDL_Surface * screen, char game_type, int piece_hovered, SDL_Rect position_cursor);
 
 /**
  * @brief Display the selected piece.
  */
-void display_piece_selected(game g, SDL_Surface * screen, char game_type, int piece_selected);
+void display_piece_selected(cgame g, SDL_Surface * screen, char game_type, int piece_selected);
 
 /**
  * @brief Displays a "Ane Rouge" game.
  */
-void display_game_ar(game g, SDL_Surface * screen);
+void display_game_ar(cgame g, SDL_Surface * screen);
 
 /**
  * @brief Displays a "Rush hour" game.
  */
-void display_game_rh(game g, SDL_Surface * screen);
+void display_game_rh(cgame g, SDL_Surface * screen);
 
 /**
  * @brief Displays the game depending on his type.
  */
-void display_game(game g, SDL_Surface * screen, char game_type);
+void display_game(cgame g, SDL_Surface * screen, char game_type);
+
+/**
+ * @brief Displays the rules of the game played
+ */
+void display_rules(cgame g, SDL_Surface * screen, char game_type);
 
 /**
  * @brief Allows the player to move a piece until he presses "Enter" or "Escape".
